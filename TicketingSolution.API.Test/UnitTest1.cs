4789d1fbfbb431151e746299025190d4855903aa
mahdi morovati <morovati155@gmail.com>
2024-09-03 22:22:52 +0330

    create TicketingSolution.API.Test.UnitTest1.Should_Return_Forecast_Results

---
+++ TicketingSolution.API.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using TicketingSolution.API.Controllers;
+using Xunit;
+
+namespace TicketingSolution.API.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Should_Return_Forecast_Results()
+    {
+        //Arrange
+        var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        var controller = new WeatherForecastController(loggerMock.Object);
+        //Act
+        var result = controller.Get();
+
+        //Assert            
+        result.ShouldNotBeNull();
+        result.Count().ShouldBeGreaterThan(1);
+    }
+}
